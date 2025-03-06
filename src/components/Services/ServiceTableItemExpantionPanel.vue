1ce8a1fe8cf58480f0ff54f6e43cda99996071a0
“Roney <roneylima@zdzcode.com.br>
2025-03-06 10:44:29 -0400

    ajuste visual

--- src/components/Services/ServiceTableItemExpantionPanel.vue
+++ src/components/Services/ServiceTableItemExpantionPanel.vue
@@ -214,15 +214,6 @@
           </v-row>
           <v-row dense class="mb-4">
             <v-col cols="12">
-              <!-- <InfoLabel
-                title="Descrição do serviço"
-                font-size="0.9"
-                :show-divider="false"
-                icon="mdi-file-outline"
-                color-icon="orange"
-                :content="item.title"
-              /> -->
-
               <div style="white-space: pre-line" v-html="item.title" />
             </v-col>
           </v-row>
