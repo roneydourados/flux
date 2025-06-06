1a35f4bfaa8fa4f39fda4ff2d1585e38582c0901
Roney Melo <68744462+roneydourados@users.noreply.github.com>
2025-06-06 10:44:18 -0400

    ver log

--- src/middleware/01.auth.global.ts
+++ src/middleware/01.auth.global.ts
@@ -2,9 +2,11 @@ export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession();
 
   if (!loggedIn.value && to.path !== "/") {
+    console.log("🚀 ~ 1 if ~ loggedIn.value:", loggedIn.value);
     return navigateTo("/");
   }
   if (to.path === "/" && loggedIn.value) {
+    console.log("🚀 ~ 2 if ~ loggedIn.value:", loggedIn.value);
     return navigateTo("/dashboard");
     // if (loggedIn.value && to.path !== "/dashboard" && to.path !== "/site") {
     //   return navigateTo("/dashboard");
